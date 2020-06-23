bc2c81c449dacca30638db3b49ba5dde82d5cd35
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-06-23 19:06:36 +0530

    env file is added

--- server/db/connectDb.js
+++ server/db/connectDb.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+
+const URI = process.env.DATABASE_URL
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://reactMongoDB-TodoApp:1J@n1997@cluster0-dt8na.mongodb.net/REACTMOGODB-TODOAPP?retryWrites=true&w=majority', {
+    await mongoose.connect(URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     })
