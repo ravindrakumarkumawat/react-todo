cb0f91e7078934ba6d521c9c4e0cd1ad1add232c
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-06-27 18:50:40 +0530

    Small components are divided into functions

--- app/src/TodoAppTasks.js
+++ app/src/TodoAppTasks.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react'
 
+function TodoInput () {
+  return (
+    <div>
+      <input
+        className='new-todo-input'
+        placeholder='Search | Create a List'
+      />
+      <button className='btn'>
+            +
+      </button>
+    </div>
+  )
+}
+
+function Lists () {
+  return (
+    <li>
+      <span>Ravindra</span>
+      <Button />
+    </li>
+  )
+}
+
+function Button () {
+  return (
+    <span>
+      <button className='btn-delete'>Delete</button>
+      <button className='btn-edit'>Edit</button>
+      <button className='btn-edit'>Save</button>
+    </span>
+  )
+}
+
 class TodoAppTasks extends Component {
   render () {
     return (
       <div className='todo-lists'>
         <h2>My Lists </h2>
-        <div>
-          <input
-            className='new-todo-input'
-            placeholder='Search | Create a List'
-          />
-          <button className='btn'>
-            +
-          </button>
-        </div>
+        <TodoInput />
         <ul>
-          <li>
-            <span>Ravindra</span>
-            <button className='btn-delete'>Delete</button>
-            <button className='btn-edit'>Edit</button>
-            <button className='btn-edit'>Save</button>
-          </li>
+          <Lists />
         </ul>
         <p className='empty-list'>
-          Pheww, List is Empty. Let's Chill & Netflix 🍕🍔🍺
+          Pheww, List is Empty. Lets Chill & Netflix 🍕🍔🍺
         </p>
       </div>
     )
