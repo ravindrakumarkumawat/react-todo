1e89e2900578b9f59d7d2352f651529cd276f808
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-06-27 19:21:37 +0530

    Lists function is modified

--- app/src/TodoAppTasks.js
+++ app/src/TodoAppTasks.js
@@ -16,20 +16,14 @@ function TodoInput () {
 
 function Lists () {
   return (
-    <li>
-      <span>Ravindra</span>
-      <Button />
-    </li>
-  )
-}
-
-function Button () {
-  return (
-    <span>
-      <button className='btn-delete'>Delete</button>
-      <button className='btn-edit'>Edit</button>
-      <button className='btn-edit'>Save</button>
-    </span>
+    <ul>
+      <li>
+        <span>Ravindra</span>
+        <button className='btn-delete'>Delete</button>
+        <button className='btn-edit'>Edit</button>
+        <button className='btn-edit'>Save</button>
+      </li>
+    </ul>
   )
 }
 
@@ -39,9 +33,7 @@ class TodoAppTasks extends Component {
       <div className='todo-lists'>
         <h2>My Lists </h2>
         <TodoInput />
-        <ul>
-          <Lists />
-        </ul>
+        <Lists />
         <p className='empty-list'>
           Pheww, List is Empty. Lets Chill & Netflix 🍕🍔🍺
         </p>
