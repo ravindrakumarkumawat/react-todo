b21b8b4272c7f95e90308c4816d0b9cd95f39353
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-27 07:32:14 +0530

    heroku deploy process set up

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ubuntu Todo App
+  ## Backend - MongoDB
+  ## Frontend - React
+  
+### Clone of functionalities of Ubuntu todo app.
+  * Having all basic todo functionalities i.e Add tasks, Delete tasks, Edit tasks.
+  * Seacrh Tasks.
+  * Can create subtasks inside each tasks.
+  * Subtasks can be scheduled.
+  * Subtasks can be prioritize.
+  * Subtasks will sort according to fixed logic.
+  * Subtasks can be mark completed.
+  * Can add notes inside each subtasks.
+
+### See the app in action here -> https://todo-react-app1.herokuapp.com/
--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const listsRouter = require('./routes/lists')
 const taskRouter = require('./routes/tasks')
@@ -19,6 +20,14 @@ app.use(express.urlencoded({ extended: false }))
 // ROUTES
 app.use('/lists', listsRouter)
 app.use('/lists/:id/tasks', taskRouter)
+
+// Production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './app/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './app/build/index.html'))
+  })
+}
 
 // Start server
 const port = process.env.PORT || 5000
